d8977b36a326ec24397e3a53cc95240dcd2ab725
unknown <ArtCrafter2999@gmail.com>
2021-11-18 21:48:13 +0200

    Оценивание тестов
    Добавленна возможность завершить тест при прасмотре всех вопросов а так же
    оценивание по кол-ву правельных ответов, в процентах и в балах
    Так же введены вариации Множественного ответа как:
    строгая (Только один вариант где выбранны все правельные ответы - верный)
    не строгая (Каждый из вариантов приносит определенную долю от правильности)

--- TestsWpf/MainWindow.xaml.cs
+++ TestsWpf/MainWindow.xaml.cs
@@ -15,12 +15,111 @@ using System.Threading;
 using System.Windows.Controls.Primitives;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using Command;
 
 namespace TestsWpf
 {
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
         public TestField CurrentTest { get; set; }
+        public List<TestModel> Tests { get; set; } = new List<TestModel>() 
+        {
+            new TestModel()
+            {
+                Question = "Тестовый вопрос (Обычный)",
+                MultipleAnswer = false,
+                Answers = new List<Answer>()
+            {
+                new Answer("вариант ответа 1 (Приавельный)") { IsCorrect = true},
+                new Answer("вариант ответа 2"),
+                new Answer("вариант ответа 3")
+            } },
+            new TestModel()
+            {
+                Question = "Тестовый вопрос (Множественный)",
+                MultipleAnswer = true,
+                Answers = new List<Answer>()
+            {
+                new Answer("вариант ответа 1 (Правельный)"){ IsCorrect = true},
+                new Answer("вариант ответа 2"),
+                new Answer("вариант ответа 3 (Тоже правельный)"){ IsCorrect = true},
+                new Answer("вариант ответа 4"),
+                new Answer("вариант ответа 5 (и этот конечно тоже)"){ IsCorrect = true}
+            } },
+            new TestModel()
+            {
+                Question = "Тестовый вопрос (Множественный, Строгий)",
+                MultipleAnswer = true,
+                StrictAnswer = true,
+                Answers = new List<Answer>()
+            {
+                new Answer("вариант ответа 1 (Правельный)"){ IsCorrect = true},
+                new Answer("вариант ответа 2"),
+                new Answer("вариант ответа 3 (Тоже правельный)"){ IsCorrect = true},
+                new Answer("вариант ответа 4")
+            } },
+        };
+        public ICommand Complite => new RelayCommand(o =>
+        {
+            if(MessageBox.Show("Вы уверены что хотите зваершить тест?","Внимание", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                UserAnswers();
+                decimal PointsSum = 0m;
+                foreach (TestButton TestButton in Wrap.Children)
+                {
+                    var Test = TestButton.DataContext as TestField;
+                    if (Test.UserAnswers.Count != 0)
+                    {
+                        if (!Test.Test.MultipleAnswer)
+                        {
+                            if(Test.UserAnswers[0].IsCorrect) PointsSum++;
+                        }
+                        else if (Test.Test.MultipleAnswer && Test.Test.StrictAnswer)
+                        {
+                            bool Correct = true;
+                            foreach (var Answer in Test.UserAnswers)
+                            {
+                                if (Answer.IsCorrect == false)
+                                {
+                                    Correct = false;
+                                    break;
+                                }
+                            }
+                            if (Correct) PointsSum++;
+                        }
+                        else
+                        {
+                            decimal Correct = 0;
+                            decimal Incorrect = 0;
+                            decimal res = 0;
+                            foreach (var Answer in Test.Test.Answers)
+                            {
+                                if (Answer.IsCorrect) Correct++;
+                                else Incorrect++;
+                            }
+                            foreach (var Answer in Test.UserAnswers)
+                            {
+                                if (Answer.IsCorrect) res += 1m / Correct;
+                                else res -= 1m / Incorrect;
+                            }
+                            if (res < 0) res = 0;
+                            PointsSum += res;
+                        }
+                    }
+                }
+                MessageBox.Show($"Тест завершён: \nПравильно отвечено на {PointsSum:0.##} вопросов из {Tests.Count}\nПроцент ответов: {(PointsSum/Tests.Count * 100):0.##}%\nПолучено балов: {(PointsSum / Tests.Count * 12):0}/12", "Тест завершён", MessageBoxButton.OK);
+            }
+        }, o => {
+            foreach (TestButton TestButton in Wrap.Children)
+            {
+                var Test = TestButton.DataContext as TestField;
+                if (Test.State == TestState.NotViewed)
+                {
+                    return false;
+                }
+            }
+            return true;
+        });
         public void UserAnswers()
         {
             if (CurrentTest != null)
@@ -58,30 +157,17 @@ namespace TestsWpf
         {
             InitializeComponent();
             DataContext = this;
-            Wrap.Children.Add(new TestButton("1", this, new TestModel()
-            {
-                Question = "Тестовый вопрос",
-                MultipleAnswer = false,
-                Answers = new List<Answer>()
-            {
-                new Answer("вариант ответа 1"),
-                new Answer("вариант ответа 2"),
-                new Answer("вариант ответа 3")
-            }
-            }));
-            Wrap.Children.Add(new TestButton("2", this, new TestModel()
-            {
-                Question = "Тестовый вопрос 2",
-                MultipleAnswer = true,
-                Answers = new List<Answer>()
-            {
-                new Answer("вариант ответа 1"),
-                new Answer("вариант ответа 2"),
-                new Answer("вариант ответа 3")
-            }
-            }));
-
+            ShowTests();
         }
+        public void ShowTests()
+        {
+            int i = 1;
+            foreach (var Test in Tests)
+            {
+                Wrap.Children.Add(new TestButton(i++.ToString(), this, Test));
+            }
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
         public void OnPropertyChanged([CallerMemberName] string prop = "")
         {
