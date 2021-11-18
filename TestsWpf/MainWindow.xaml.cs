874a651b87320b8b3afccfcbc2564e650c92020b
unknown <ArtCrafter2999@gmail.com>
2021-11-18 14:59:11 +0200

    Визуализация тестов
    Тесты теперь обрели визуализацию:
     Из кода можно тесту задать:
      вопрос, ответы, Опцию "множественные ответы"
    даже можно задать правильный ответ но подсчёта правельных ответов пока нет

---
+++ TestsWpf/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Threading;
+using System.Windows.Controls.Primitives;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TestsWpf
+{
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public TestField CurrentTest { get; set; }
+        public void UserAnswers()
+        {
+            if (CurrentTest != null)
+            {
+                bool NotToggled = true;
+                int id = 0;
+                foreach (UIElement button in CurrentTest.Stack.Children)
+                {
+                    ToggleButton toggleButton = button as ToggleButton;
+                    if (toggleButton != null)
+                    {
+                        if (toggleButton.IsChecked.Value)
+                        {
+                            CurrentTest.UserAnswers.Add(CurrentTest.Test.Answers.Find(o=> o == toggleButton.Content));
+                            NotToggled = false;
+                        }
+                        else
+                        {
+                            id++;
+                        }
+                    }
+                }
+                if (NotToggled)
+                {
+                    CurrentTest.State = TestState.Skipped;
+                }
+                else
+                {
+                    CurrentTest.State = TestState.Answered;
+                }
+                CurrentTest.OnPropertyChanged("ButtonColor");
+            }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            Wrap.Children.Add(new TestButton("1", this, new TestModel()
+            {
+                Question = "Тестовый вопрос",
+                MultipleAnswer = false,
+                Answers = new List<Answer>()
+            {
+                new Answer("вариант ответа 1"),
+                new Answer("вариант ответа 2"),
+                new Answer("вариант ответа 3")
+            }
+            }));
+            Wrap.Children.Add(new TestButton("2", this, new TestModel()
+            {
+                Question = "Тестовый вопрос 2",
+                MultipleAnswer = true,
+                Answers = new List<Answer>()
+            {
+                new Answer("вариант ответа 1"),
+                new Answer("вариант ответа 2"),
+                new Answer("вариант ответа 3")
+            }
+            }));
+
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ TestsWpf/TestField.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using Command;
+
+namespace TestsWpf
+{
+    public enum TestState
+    {
+        NotViewed,
+        Slected,
+        Skipped,
+        Answered
+    }
+    public partial class TestField : UserControl, INotifyPropertyChanged
+    {
+        private TestState _state = TestState.NotViewed;
+        public TestState State { get => _state; set { _state = value; OnPropertyChanged("ButtonColor"); } }
+        public ICommand SelectCommand => new RelayCommand(o =>
+        {
+            From.UserAnswers();
+            From.CurrentTest = this;
+            State = TestState.Slected;
+            OnPropertyChanged("ButtonColor");
+            From.OnPropertyChanged("CurrentTest");
+        });
+        public string ButtonTitle { get; set; }
+        public string ButtonColor
+        {
+            get
+            {
+                switch (_state)
+                {
+                    case TestState.NotViewed:
+                        return "WhiteSmoke";
+                    case TestState.Slected:
+                        return "Blue";
+                    case TestState.Skipped:
+                        return "DarkGray";
+                    case TestState.Answered:
+                        return "LightGreen";
+                    default:
+                        return "WhiteSmoke";
+                }
+            }
+        }
+        public List<Answer> UserAnswers = new List<Answer>();
+        public MainWindow From;
+        public TestModel Test;
+        public TestField(MainWindow from, TestModel Question)
+        {
+            InitializeComponent();
+            DataContext = this;
+            From = from;
+            Test = Question;
+            ReloadTest();
+        }
+        public void ReloadTest()
+        {
+            Stack.Children.Add(new TextBlock() { Text = Test.Question, IsHyphenationEnabled = true, FontSize=20 });
+            foreach (var Answer in Test.Answers)
+            {
+                if (Test.MultipleAnswer)
+                {
+                    Stack.Children.Add(new CheckBox() { Content = Answer, FontSize = 16 });
+                }
+                else
+                {
+                    Stack.Children.Add(new RadioButton() { Content = Answer, FontSize = 16 });
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
